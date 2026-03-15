2ef0434019868bd1cebf5085006cdba75abf8b5c
Krywion <krywionek.maks@gmail.com>
2026-03-15 15:35:15 +0100

    feat(SKY-19): add UserController (/api/me) returning JWT user info

---
+++ skyroster-backend/src/main/java/pl/skyroster/skyroster_backend/infrastructure/adapter/in/web/UserController.java
@@ -0,0 +1,36 @@
+package pl.skyroster.skyroster_backend.infrastructure.adapter.in.web;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.skyroster.skyroster_backend.generated.api.ApiApi;
+import pl.skyroster.skyroster_backend.generated.model.UserInfo;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @GetMapping(ApiApi.PATH_GET_CURRENT_USER)
+    public ResponseEntity<UserInfo> getCurrentUser() {
+        JwtAuthenticationToken authentication = (JwtAuthenticationToken)
+                SecurityContextHolder.getContext().getAuthentication();
+        Jwt jwt = authentication.getToken();
+
+        List<String> roles = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .map(a -> a.replace("ROLE_", ""))
+                .toList();
+
+        UserInfo userInfo = new UserInfo()
+                .username(jwt.getClaimAsString("preferred_username"))
+                .email(jwt.getClaimAsString("email"))
+                .roles(roles);
+
+        return ResponseEntity.ok(userInfo);
+    }
+}
